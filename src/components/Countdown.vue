b1f88a24cff118e33da1e5293d693185d645be40
Gabriel Dunn <gabe.dunn@shaw.ca>
2018-09-14 12:18:02 -0600

    remove unused function in countdown

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -23,14 +23,6 @@
         const days = Math.floor(dif / (1000 * 60 * 60 * 24))
         const hours = Math.floor((dif / (1000 * 60 * 60)) % 24)
         return `${days}d ${hours}h ${dif.getMinutes()}m ${dif.getSeconds()}s`
-      },
-      calculateDate2 () {
-        const total = new Date(Date.parse(this.date) - this.currentDate)
-        const seconds = Math.floor((total / 1000) % 60)
-        const minutes = Math.floor((total / 1000 / 60) % 60)
-        const hours = Math.floor((total / (1000 * 60 * 60)) % 24)
-        const days = Math.floor(total / (1000 * 60 * 60 * 24))
-        return `${parseInt(days, 10)}d ${parseInt(hours, 10)}h ${parseInt(minutes, 10)}m ${parseInt(seconds, 10)}s`
       }
     },
     mounted () {
